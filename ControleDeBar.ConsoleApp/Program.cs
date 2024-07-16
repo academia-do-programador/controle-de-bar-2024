9504d0999d20028a7aa1205e8f94d98763f273de
Tiago Santini <tiagosantiniathayde@outlook.com>
2024-07-16 16:17:13 -0300

    Adicionado projeto de console

---
+++ ControleDeBar.ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using ControleDeBar.Dominio.ModuloMesa;
+using ControleDeBar.Infra.Orm.Compartilhado;
+using ControleDeBar.Infra.Orm.ModuloMesa;
+
+namespace ControleDeBar.ConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ControleDeBarDbContext dbContext = new ControleDeBarDbContext();
+            RepositorioMesaEmOrm repositorioMesa = new RepositorioMesaEmOrm(dbContext);
+
+            List<Mesa> mesasCadastradas = repositorioMesa.SelecionarTodos();
+
+            foreach (Mesa mesa in mesasCadastradas)
+                Console.WriteLine($"Id: {mesa.Id}, Numero: {mesa.Numero}, Ocupada: {mesa.Ocupada}");
+
+            Console.ReadLine();
+        }
+    }
+}
