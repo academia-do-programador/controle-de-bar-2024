c8a03e28aabfcaea7c58d229b789a9b8da8d140c
Tiago Santini <tiagosantiniathayde@outlook.com>
2024-07-25 15:00:50 -0300

    Implementado controlador de produtos

---
+++ ControleDeBar.WebApp/Controllers/ProdutoController.cs
@@ -0,0 +1,111 @@
+﻿using ControleDeBar.Dominio.ModuloProduto;
+using ControleDeBar.Infra.Orm.Compartilhado;
+using ControleDeBar.Infra.Orm.ModuloProduto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeBar.WebApp.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public ViewResult Listar()
+        {
+            var db = new ControleDeBarDbContext();
+            var repositorioProduto = new RepositorioProdutoEmOrm(db);
+
+            var produtos = repositorioProduto.SelecionarTodos();
+
+            ViewBag.Produtos = produtos;
+
+            return View();
+        }
+
+        public ViewResult Inserir()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult Inserir(Produto novoProduto)
+        {
+            var db = new ControleDeBarDbContext();
+            var repositorioProduto = new RepositorioProdutoEmOrm(db);
+
+            repositorioProduto.Inserir(novoProduto);
+
+            ViewBag.Mensagem = $"O registro com o ID {novoProduto.Id} foi cadastrado com sucesso!";
+            ViewBag.Link = "/produto/listar";
+
+            HttpContext.Response.StatusCode = 201;
+
+            return View("mensagens");
+        }
+
+        public ViewResult Editar(int id)
+        {
+            var db = new ControleDeBarDbContext();
+            var repositorioProduto = new RepositorioProdutoEmOrm(db);
+
+            var produto = repositorioProduto.SelecionarPorId(id);
+
+            ViewBag.Produto = produto;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult Editar(int id, Produto produtoAtualizado)
+        {
+            var db = new ControleDeBarDbContext();
+            var repositorioProduto = new RepositorioProdutoEmOrm(db);
+
+            var produtoOriginal = repositorioProduto.SelecionarPorId(id);
+
+            repositorioProduto.Editar(produtoOriginal, produtoAtualizado);
+
+            ViewBag.Mensagem = $"O registro com o ID {produtoOriginal.Id} foi editado com sucesso!";
+            ViewBag.Link = "/produto/listar";
+
+            return View("mensagens");
+        }
+
+        public ViewResult Excluir(int id)
+        {
+            var db = new ControleDeBarDbContext();
+            var repositorioProduto = new RepositorioProdutoEmOrm(db);
+
+            var produto = repositorioProduto.SelecionarPorId(id);
+
+            ViewBag.Produto = produto;
+
+            return View();
+        }
+
+        [HttpPost, ActionName("excluir")]
+        public ViewResult ExcluirConfirmado(int id)
+        {
+            var db = new ControleDeBarDbContext();
+            var repositorioProduto = new RepositorioProdutoEmOrm(db);
+
+            var produto = repositorioProduto.SelecionarPorId(id);
+
+            repositorioProduto.Excluir(produto);
+
+            ViewBag.Mensagem = $"O registro com o ID {produto.Id} foi excluído com sucesso!";
+            ViewBag.Link = "/produto/listar";
+
+            return View("mensagens");
+        }
+
+        public ViewResult Detalhes(int id)
+        {
+            var db = new ControleDeBarDbContext();
+            var repositorioProduto = new RepositorioProdutoEmOrm(db);
+
+            var produto = repositorioProduto.SelecionarPorId(id);
+
+            ViewBag.Produto = produto;
+
+            return View();
+        }
+    }
+}
