cab9e3ae4306497a026718675c9dc118971605e2
Tiago Santini <tiagosantiniathayde@outlook.com>
2024-07-25 15:53:02 -0300

    Adicionado controlador de conta

---
+++ ControleDeBar.WebApp/Controllers/ContaController.cs
@@ -0,0 +1,27 @@
+﻿using ControleDeBar.Dominio.ModuloConta;
+using ControleDeBar.Infra.Orm.Compartilhado;
+using ControleDeBar.Infra.Orm.ModuloConta;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeBar.WebApp.Controllers
+{
+    public class ContaController : Controller
+    {
+        public ViewResult Listar(TipoFiltroContaEnum status)
+        {
+            var db = new ControleDeBarDbContext();
+            var repositorioConta = new RepositorioContaEmOrm(db);
+
+            var contas = status switch
+            {
+                TipoFiltroContaEnum.Abertas => repositorioConta.SelecionarContasEmAberto(),
+                TipoFiltroContaEnum.Fechadas => repositorioConta.SelecionarContasFechadas(),
+                _ => repositorioConta.SelecionarContas()
+            };
+
+            ViewBag.Contas = contas;
+
+            return View();
+        }
+    }
+}
